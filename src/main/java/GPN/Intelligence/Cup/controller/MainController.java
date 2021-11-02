e503d5e0ff3f07a8f2123f1d63791fa00cc9552e
alexeyder <hottelmich@gmail.com>
2021-11-02 20:03:56 +0700

    Replace Gradle to Maven

--- src/main/java/GPN/Intelligence/Cup/controller/MainController.java
+++ src/main/java/GPN/Intelligence/Cup/controller/MainController.java
@@ -10,4 +10,20 @@ public class MainController {
     public String index() {
         return "Greetings!";
     }
+
+//    @GetMapping("/get")
+//    public String getSoap() {
+//        String request = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+//                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+//                "  <soap:Body>\n" +
+//                "    <Add xmlns=\"http://tempuri.org/\">\n" +
+//                "      <intA>int</intA>\n" +
+//                "      <intB>int</intB>\n" +
+//                "    </Add>\n" +
+//                "  </soap:Body>\n" +
+//                "</soap:Envelope>";
+//
+//
+//        return "Greetings!";
+//    }
 }
