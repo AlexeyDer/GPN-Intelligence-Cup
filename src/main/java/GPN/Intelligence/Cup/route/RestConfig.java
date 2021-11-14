789d459e177acd6fb4792fdf659cf62ac170a2e8
alexeyder <hottelmich@gmail.com>
2021-11-14 20:08:03 +0700

    Add routes to others operations, make exception clause, start to understand caching

--- src/main/java/GPN/Intelligence/Cup/route/RestConfig.java
+++ src/main/java/GPN/Intelligence/Cup/route/RestConfig.java
@@ -3,8 +3,7 @@ package GPN.Intelligence.Cup.route;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
-import org.tempuri.Add;
-import org.tempuri.AddResponse;
+import org.tempuri.*;
 
 @Component
 public class RestConfig extends RouteBuilder {
@@ -12,23 +11,38 @@ public class RestConfig extends RouteBuilder {
     @Override
     public void configure() {
         restConfiguration()
-                .component("servlet")
-                .port(8080).bindingMode(RestBindingMode.auto)
+                .component("servlet").host("0.0.0.0").port(8080).bindingMode(RestBindingMode.auto).scheme("http")
                 .dataFormatProperty("prettyPrint", "true")
                 .contextPath("/")
                 .apiContextPath("/api-doc")
                 .apiProperty("api.title", "Camel2Soap")
                 .apiProperty("api.version", "1.0")
-                .apiProperty("host", "")
-                .enableCORS(true);
+                .apiProperty("host","");
 
         rest("/api").description("Calculator rest service")
                 .consumes("application/json")
                 .produces("application/json")
-                .post("/add").description("Add request")
+                .post("/add")
+                    .description("Adds two integers")
                     .type(Add.class)
                     .outType(AddResponse.class)
-                .to("direct:add");
+                .to("direct:add")
+                .post("/divide")
+                    .description("Divide two integers")
+                    .type(Divide.class)
+                    .outType(DivideResponse.class)
+                .to("direct:divide")
+                .post("/multiply")
+                    .description("Multiply two integers")
+                    .type(Multiply.class)
+                    .outType(MultiplyResponse.class)
+                .to("direct:multiply")
+                .post("/subtract")
+                    .description("Subtract two integers")
+                    .type(Subtract.class)
+                    .outType(SubtractResponse.class)
+                .to("direct:subtract");
+
 
     }
 }
--- src/main/java/GPN/Intelligence/Cup/route/SoapToRest.java
+++ src/main/java/GPN/Intelligence/Cup/route/SoapToRest.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.apache.camel.Exchange;
-import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.cxf.common.message.CxfConstants;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.cxf.message.MessageContentsList;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.tempuri.Add;
-import org.tempuri.AddResponse;
+import org.tempuri.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,34 +20,131 @@ public class SoapToRest extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("direct:add")
-                .streamCaching()
-                .removeHeaders("CamelHttp*")
-                .doTry()
-                    .process(exchange -> {
-                        Add add = exchange.getIn().getBody(Add.class);
-                        List<Object> params = new ArrayList<>();
-                        params.add(add.getIntA());
-                        params.add(add.getIntB());
 
-                        exchange.getIn().setBody(params);
-                    })
-                .doCatch(Exception.class, InvalidFormatException.class)
-                    .log(LoggingLevel.WARN, "exception")
-                    .process(exchange -> {
-                        exchange.getIn().setBody("{'error': Bad Request}");
-                        exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "application/json");
-                        exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
-                    })
-                    .to("mock:catch")
-                .end()
+        getCamelContext().setStreamCaching(true);
+
+        onException(ArithmeticException.class).handled(true)
+                .process(exchange -> {
+                    exchange.getIn().setBody("{\"error\": \"\"Division by zero is not allowed\"}\"");
+                    exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "application/json");
+                    exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+                })
+                .end();
+
+        onException(IllegalArgumentException.class).handled(true)
+                .process(exchange -> {
+                    exchange.getIn().setBody("{\"error\": \"Bad Request}\"");
+                    exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "application/json");
+                    exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+                })
+                .end();
+
+        from("direct:add")
+                .removeHeaders("CamelHttp*")
+                .process(exchange -> {
+                    Add add = exchange.getIn().getBody(Add.class);
+                    List<Object> params = new ArrayList<>();
+                    params.add(add.getIntA());
+                    params.add(add.getIntB());
+
+                    exchange.getIn().setBody(params);
+                })
                 .setHeader(CxfConstants.OPERATION_NAME, constant("{{endpoint.operation.add}}"))
                 .setHeader(CxfConstants.OPERATION_NAMESPACE, constant("{{endpoint.namespace}}"))
                 .to("cxf:bean:cxfCalculator")
                 .process(exchange -> {
                     MessageContentsList response = (MessageContentsList) exchange.getIn().getBody();
                     AddResponse r = new AddResponse();
-                    r.setAddResult((Integer)response.get(0));
+                    r.setAddResult((Integer) response.get(0));
+
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+                    String res = objectMapper.writeValueAsString(r);
+
+                    exchange.getIn().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+                    exchange.getIn().setBody(res);
+                })
+                .unmarshal().json(JsonLibrary.Jackson)
+                .to("mock:output");
+
+
+        from("direct:divide")
+                .removeHeaders("CamelHttp*")
+                .process(exchange -> {
+                    Divide div = exchange.getIn().getBody(Divide.class);
+                    List<Object> params = new ArrayList<>();
+                    int intA = div.getIntA();
+                    int intB = div.getIntB();
+                    if (intB != 0) {
+                        params.add(intA);
+                        params.add(intB);
+                        exchange.getIn().setBody(params);
+                    } else {
+                        throw new ArithmeticException();
+                    }
+                })
+                .setHeader(CxfConstants.OPERATION_NAME, constant("{{endpoint.operation.divide}}"))
+                .setHeader(CxfConstants.OPERATION_NAMESPACE, constant("{{endpoint.namespace}}"))
+                .to("cxf:bean:cxfCalculator")
+                .process(exchange -> {
+                    MessageContentsList response = (MessageContentsList) exchange.getIn().getBody();
+                    DivideResponse r = new DivideResponse();
+                    r.setDivideResult((Integer) response.get(0));
+
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+                    String res = objectMapper.writeValueAsString(r);
+
+                    exchange.getIn().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+                    exchange.getIn().setBody(res);
+                })
+                .unmarshal().json(JsonLibrary.Jackson)
+                .to("mock:output");
+
+        from("direct:multiply")
+                .removeHeaders("CamelHttp*")
+                .process(exchange -> {
+                    Multiply mult = exchange.getIn().getBody(Multiply.class);
+                    List<Object> params = new ArrayList<>();
+                    params.add(mult.getIntA());
+                    params.add(mult.getIntB());
+                    exchange.getIn().setBody(params);
+                })
+                .setHeader(CxfConstants.OPERATION_NAME, constant("{{endpoint.operation.multiply}}"))
+                .setHeader(CxfConstants.OPERATION_NAMESPACE, constant("{{endpoint.namespace}}"))
+                .to("cxf:bean:cxfCalculator")
+                .process(exchange -> {
+                    MessageContentsList response = (MessageContentsList) exchange.getIn().getBody();
+                    MultiplyResponse r = new MultiplyResponse();
+                    r.setMultiplyResult((Integer) response.get(0));
+
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+                    String res = objectMapper.writeValueAsString(r);
+
+                    exchange.getIn().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+                    exchange.getIn().setBody(res);
+                })
+                .unmarshal().json(JsonLibrary.Jackson)
+                .to("mock:output");
+
+        from("direct:subtract")
+                .removeHeaders("CamelHttp*")
+                .process(exchange -> {
+                    Subtract sub = exchange.getIn().getBody(Subtract.class);
+                    List<Object> params = new ArrayList<>();
+                    params.add(sub.getIntA());
+                    params.add(sub.getIntB());
+                    exchange.getIn().setBody(params);
+                })
+                .setHeader(CxfConstants.OPERATION_NAME, constant("{{endpoint.operation.subtract}}"))
+                .setHeader(CxfConstants.OPERATION_NAMESPACE, constant("{{endpoint.namespace}}"))
+                .to("cxf:bean:cxfCalculator")
+                .process(exchange -> {
+                    MessageContentsList response = (MessageContentsList) exchange.getIn().getBody();
+                    SubtractResponse r = new SubtractResponse();
+                    r.setSubtractResult((Integer) response.get(0));
+
                     ObjectMapper objectMapper = new ObjectMapper();
                     objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
                     String res = objectMapper.writeValueAsString(r);
@@ -60,4 +155,5 @@ public class SoapToRest extends RouteBuilder {
                 .unmarshal().json(JsonLibrary.Jackson)
                 .to("mock:output");
     }
+
 }
